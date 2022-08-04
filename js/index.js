6a5ed83e2723e4ed4c08cfd3811a2aa595d47b77
Rurikko <62918162+Reiz-L@users.noreply.github.com>
2022-08-04 11:39:27 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,3 +2,26 @@ function search_by_google(){
 	var inputDom = document.getElementById("search_text").value;
 	window.open('https://xn--flw351e.ml/search?q=' + inputDom);
 }
+
+function onsomekey(){
+	document.onkeydown = function(e){
+		if (!e) e = window.event;
+		if ((e.keyCode || e.which) == 13) {
+			var obtnLogin = document.getElementById("search_button");
+		    obtnLogin.focus();　　　　　　　　　 fun();//提交按钮触发的方法
+		}
+	}
+}
+
+function show(){
+var date = new Date(); //日期对象
+var now = "";
+now = date.getFullYear()+"年"; //读英文就行了
+now = now + (date.getMonth()+1)+"月"; //取月的时候取的是当前月-1如果想取当前月+1就可以了
+now = now + date.getDate()+"日";
+now = now + date.getHours()+"时";
+now = now + date.getMinutes()+"分";
+now = now + date.getSeconds()+"秒";
+document.getElementById("time_p").innerHTML = now; //div的html是now这个字符串
+setTimeout("show()",1000); //设置过1000毫秒就是1秒，调用show方法
+}
